71281652f3e8c413804fae5bb69f2d0e707ddb46
MichalPoniatowski <m.poniatowski87@gmail.com>
2023-08-07 22:18:38 +0200

    localstorage getting and setting added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer, filterReducer } from './reducer';
 
+const savedContacts = localStorage.getItem('contacts');
+
+const preloadedState = savedContacts
+  ? { contacts: JSON.parse(savedContacts) }
+  : {};
+
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
   },
+  preloadedState,
+});
+
+store.subscribe(() => {
+  localStorage.setItem('contacts', JSON.stringify(store.getState().contacts));
 });
